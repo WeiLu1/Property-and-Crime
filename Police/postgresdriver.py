b59769c67fa50f9c721f4d627b5896bbb3ad28c6
WeiLu1 <w.j.lu96@gmail.com>
2021-01-23 11:44:22 +0000

    batch insert api response method

--- Police/postgresdriver.py
+++ Police/postgresdriver.py
@@ -1,5 +1,6 @@
 import psycopg2
-from queries import create_crimes_table
+import psycopg2.extras
+from queries import create_crimes_table, insert_crime, create_crimes_index
 import os
 from dotenv import load_dotenv
 
@@ -10,7 +11,7 @@ host = os.getenv('POSTGRES_HOST')
 port = os.getenv('POSTGRES_PORT')
 
 
-class PostgresDriver(object):
+class Postgres(object):
 
     def __init__(self):
         try:
@@ -22,18 +23,28 @@ class PostgresDriver(object):
 
     def execute_query(self, query):
         self.cursor.execute(query)
+        self.conn.commit()
+
+    def batch_insert(self, batch):
+        psycopg2.extras.execute_batch(self.cursor, insert_crime, batch)
+        self.conn.commit()
 
     def disconnect(self):
         self.conn.close()
         self.cursor.close()
 
-    def setup(self):
+    def setup_initial(self):
+        """
+        Only needs to be done once.
+        :return: Table created in Postgres instance on server
+        """
         self.execute_query(create_crimes_table)
+        self.execute_query(create_crimes_index)
         self.conn.commit()
         self.disconnect()
 
 
 if __name__ == '__main__':
-    db = PostgresDriver()
-    db.setup()
+    db = Postgres()
+    db.setup_initial()
 
