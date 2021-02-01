d113625cbc32035206130720af85c0a2ccc1a57d
WeiLu1 <w.j.lu96@gmail.com>
2021-02-01 21:25:20 +0000

    new page to select borough and month crimes

--- Flask/run.py
+++ Flask/run.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for
 import psycopg2
 import plotly.express as px
 import plotly.utils
+from datetime import datetime
 import json
 import os
 from dotenv import load_dotenv
@@ -31,23 +32,19 @@ properties_all_query = """
 SELECT borough, numberbeds, price FROM propertiesaverage ORDER BY borough ASC, numberbeds ASC;
 """
 
+dates_query = """
+SELECT DISTINCT(date) FROM crimes ORDER BY date DESC;
+"""
 
-def db_data(borough='', properties=False, crimes=False, all=False):
+crime_date_query = """
+SELECT category, COUNT(DISTINCT(id)) FROM crimes WHERE borough LIKE '{}' AND date = '{}' GROUP BY category ORDER BY COUNT(DISTINCT(id)) DESC;
+"""
+
+
+def db_data(query):
     conn = psycopg2.connect(database=database, user=user, host=host, port=port)
     cursor = conn.cursor()
-
-    if all:
-        cursor.execute(crime_all_query)
-        data_crime = cursor.fetchall()
-        cursor.execute(properties_all_query)
-        data_property = cursor.fetchall()
-        return data_crime, data_property
-
-    if properties:
-        cursor.execute(properties_query.format(borough))
-    if crimes:
-        cursor.execute(crime_query.format(borough))
-
+    cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
     return data
@@ -66,18 +63,19 @@ def hello():
         if not borough:
             return render_template("home.html")
 
-        properties = db_data(borough=borough, properties=True)
-        crimes = db_data(borough=borough, crimes=True)
+        properties = db_data(properties_query.format(borough))
+        crimes = db_data(crime_query.format(borough))
 
         return render_template("home.html", borough=borough, properties=properties, crimes=crimes)
     else:
         return render_template("home.html")
 
 
-@app.route("/compare")
+@app.route("/compare", methods=['GET'])
 def compare():
     if request.method == 'GET':
-        crime, properties = db_data(all=True)
+        crime = db_data(crime_all_query)
+        properties = db_data(properties_all_query)
 
         line_property = px.line(properties, x=[x[1] for x in properties], y=[x[2] for x in properties], color=[x[0] for x in properties])
         line_property = line_property.update_traces(mode='markers+lines')
@@ -104,6 +102,22 @@ def compare():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/crimes-by-month", methods=['GET', 'POST'])
+def crime_selector():
+    dates = [x[0] for x in db_data(dates_query)]
+
+    if request.method == 'POST':
+        borough = request.form['dropdownborough']
+        if not borough:
+            return render_template("crime_selector.html", dates=dates)
+
+        specific_date = request.form['dropdowndate']
+        crime_date_data = db_data(crime_date_query.format(borough, specific_date))
+        return render_template("crime_selector.html", dates=dates, borough=borough, specificdate=specific_date, crimedatemonths=crime_date_data)
+
+    return render_template("crime_selector.html", dates=dates)
 
 
 if __name__ == "__main__":
